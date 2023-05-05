1222a4dec8419cfd4ea54c70b3c6e640e56d8f3c
Darius3001 <darius.mertin@outlook.com>
2023-05-05 14:10:58 +0200

    small style adjustment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,8 +21,8 @@ const Home: NextPage = () => {
         </nav>
         <div className="flex item-center h-screen ">
           <textarea defaultValue={text} className="
-            place-self-center h-5/6 w-1/3 mx-auto text-center 
-            resize-none border rounded
+            place-self-center h-5/6 w-1/3 mx-auto text-center p-2
+            resize-none border rounded-md
             focus:outline-none focus:border-black transition duration-75 ease-in-out">
           </textarea>
         </div>
